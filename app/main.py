d6fed75be14fbdcd0959146a860eab76a12cc60c
Miguel Llamas Sáez <miguel.llamas@jobandtalent.com@Miguels-MacBook-Pro.local>
2025-10-21 20:05:08 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ app = FastAPI()
 @app.post("/slack/events")
 async def slack_events(req: Request, background_tasks: BackgroundTasks):
     body = await req.json()
+    if body.get("type") == "url_verification":
+        print("Verification request from Slack")
+        return {"challenge": body["challenge"]}
     background_tasks.add_task(handler, body)
     return {"ok": True}
 
