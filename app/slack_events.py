b91963a139e0f4ff4d2fada0f82d59d7286b0598
Miguel Llamas Sáez <miguel.llamas@jobandtalent.com@Miguels-MacBook-Pro.local>
2025-10-21 19:27:54 +0200

    added slack ver and build yaml improved

--- app/slack_events.py
+++ app/slack_events.py
@@ -9,7 +9,7 @@ processed_events = set()
 def handler(body: dict):
     if body.get("type") == "url_verification":
         logger.debug("Verification request from Slack")
-        return {"challenge":body["challenge"]}
+        return json.dumps({"challenge" : body["challenge"]})
 
     event = body.get("event", {})
     event_id = body.get("event_id")
